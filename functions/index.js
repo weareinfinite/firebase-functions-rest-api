d880b0d6aec2f7889bf9ce9c398e1095d964d8de
Sarath <saratonite@gmx.com>
2018-02-19 11:57:02 +0530

    :fire: Installed Express for handling functions http request

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
-
+const express = require('express');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.json({message: 'Hello'})
+})
+
+exports.helloWorld = functions.https.onRequest(app);

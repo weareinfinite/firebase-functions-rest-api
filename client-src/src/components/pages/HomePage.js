2a181b04092789286d35c1c4007f99feaebbb14d
Sarath <saratonite@gmx.com>
2018-03-05 15:28:38 +0530

    :smile: Login page update , firebase.json updated

--- client-src/src/components/pages/HomePage.js
+++ client-src/src/components/pages/HomePage.js
@@ -1,15 +1,26 @@
-import React from 'react';
-const HomePage = () => (
-    <div className="empty">
-        <div className="empty-icon">
-            <i className="icon icon-people"></i>
-        </div>
-        <p className="empty-title h5">Welcome to Firebase Client App</p>
-        <p className="empty-subtitle">Click the button to start a conversation.</p>
-        <div className="empty-action">
-            <button className="btn btn-primary">Send a message</button>
-        </div>
-    </div>
-)
+import React, { Component } from 'react';
+export default class  HomePage extends Component {
+    constructor(props) {
+        super(props);
 
-export default HomePage;
+        document.title = 'Home';
+    }
+   render (){
+
+        return (
+            <div className="empty">
+                <div className="empty-icon">
+                    <i className="icon icon-people"></i>
+                </div>
+                <p className="empty-title h5">Welcome to Firebase Client App</p>
+                <p className="empty-subtitle">Click the button to start a conversation.</p>
+                <div className="empty-action">
+                    <button className="btn btn-primary">Send a message</button>
+                </div>
+            </div>
+             
+        )
+
+   }
+
+}
--- client-src/src/components/pages/LoginPage.js
+++ client-src/src/components/pages/LoginPage.js
@@ -1,11 +1,57 @@
-import React from 'react';
-const LoginPage = () => (
-    <div className="empty">
-        <div className="empty-icon">
-            <i className="icon icon-people"></i>
-        </div>
-        <p className="empty-title h5">Login page</p>
-    </div>
-)
+import React, { Component } from 'react';
 
-export default LoginPage;
+export default class  LoginPage extends Component {
+
+    constructor(props) {
+
+        super(props)
+
+        this.state = {
+            email: 'sarath@gmail.com',
+            password: 'somepass',
+            errors: null
+        }
+
+        this.inputChange = this.inputChange.bind(this);
+
+        document.title = 'Login';
+
+    }
+
+    inputChange(e) {
+
+        this.setState({[e.target.name]: e.target.value })
+    }
+
+    render() {
+        return(
+           <div className="container login-form-container">
+        <form>
+        <h2>Login</h2>
+        <div className="form-group has-success">
+            <label className="form-label" htmlFor="input-example-1">Email</label>
+            <input className="form-input" type="text" name="email" onChange={ this.inputChange } value={ this.state.email } placeholder="Email" />
+            <p className="form-input-hint">The email is invalid.</p>
+        </div>
+        <div className="form-group">
+            <label className="form-label" htmlFor="input-example-1">Password</label>
+            <input className="form-input is-success" type="password" name="password" onChange={ this.inputChange } value={ this.state.password }  placeholder="Password"/>
+            <p className="form-input-hint">The password is invalid.</p>
+        </div>
+
+       
+        <div className="form-group">
+            <label className="form-checkbox is-error">
+            <input type="checkbox" />
+            <i className="form-icon"></i> Remember me
+            </label>
+        </div>
+        <div className="form-group">
+            <button className="btn btn-primary">LOGIN</button>
+        </div>
+        </form>
+    </div> 
+        )
+    }
+}
+    
